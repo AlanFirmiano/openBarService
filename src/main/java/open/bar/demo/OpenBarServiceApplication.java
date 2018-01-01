09a4106ebc94c51263e64f86875dd5350d4b18fc
alanfirmiano <alanfirmiano18@gmail.com>
2018-01-01 13:23:38 -0300

    first commit

---
+++ src/main/java/open/bar/demo/OpenBarServiceApplication.java
@@ -0,0 +1,12 @@
+package open.bar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBarServiceApplication.class, args);
+	}
+}
